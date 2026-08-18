1add2bd525d9bbdff3771e453b5fbd4d7a89bef2
therunningman222 <61023823+therunningman222@users.noreply.github.com>
2026-08-18 21:51:44 +0100

    Add task edit script to PWA cache

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='spaceship-you-v6';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./delete.js','./sync.js','./decay.js'];
+const CACHE='spaceship-you-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./delete.js','./sync.js','./decay.js','./edit.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   if(e.request.mode==='navigate'){
     e.respondWith(fetch(e.request).then(r=>r.text().then(text=>{
-      const injected=text.replace('</body>','<script src="./sync.js"></script><script src="./delete.js"></script><script src="./decay.js"></script></body>');
+      const injected=text.replace('</body>','<script src="./sync.js"></script><script src="./delete.js"></script><script src="./decay.js"></script><script src="./edit.js"></script></body>');
       const response=new Response(injected,{status:r.status,statusText:r.statusText,headers:r.headers});
       caches.open(CACHE).then(c=>c.put(e.request,response.clone()));
       return response;
