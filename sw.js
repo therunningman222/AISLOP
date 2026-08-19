5a5c1a1b26dd0e422a4015331fb9e062bd246dc0
therunningman222 <61023823+therunningman222@users.noreply.github.com>
2026-08-19 11:40:45 +0100

    Bump cache after room average loop fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spaceship-you-v23';
+const CACHE='spaceship-you-v24';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./delete.js','./sync.js','./decay.js','./edit.js','./cloud-save.js','./sort.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
